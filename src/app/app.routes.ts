69314135534021770d85c9a13d42d50a67510865
Hiohana Dall Agnol <hio_hana@hotmail.com>
2024-10-09 17:05:51 -0300

    ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
 
 export const routes: Routes = [
-    {path: 'task-list', title:'Lista de Tarefas', component: TaskListComponent},
-    {path: 'task-details/:id', title:'Detalhes', component:TaskDetailsComponent},
+    {path: 'task-list', title:'Task Manager', component: TaskListComponent},
+    {path: 'task-details/:id', title:'Details', component:TaskDetailsComponent},
     {path: '', redirectTo:'/task-list', pathMatch:'full'}
 ];
